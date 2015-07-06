3228ccce40e9ff2405db1a32b4c3279ccc7ae478
David McMackins II <contact@mcmackins.org>
2015-07-05 22:28:26 -0500

    Added Piece class for player pieces

---
+++ piece.lua
@@ -0,0 +1,35 @@
+--
+--  Agario Checkers - Checkers-like game with inspiration from agar.io
+--  Copyright (C) 2015 Delwink, LLC
+--
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU Affero General Public License as published by
+--  the Free Software Foundation, version 3 only.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU Affero General Public License for more details.
+--
+--  You should have received a copy of the GNU Affero General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+require 'class'
+
+Piece = class()
+
+function Piece:__init(x, y)
+   self.x = x
+   self.y = y
+   self.size = 1
+end
+
+function Piece:move(dtx, dty)
+   self.x = self.x + dtx
+   self.y = self.y + dty
+end
+
+function Piece:resize(dt)
+   self.size = self.size + dt
+end
